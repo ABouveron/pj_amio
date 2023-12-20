e362bf9a71cd64880ba890e1a822a33609e2a7c7
unknown <hind.latrache@telecomnancy.eu>
2023-12-20 17:54:37 +0100

    test boot and service

--- app/src/main/java/fr/abouveron/projectamio/BootBroadcastReceiver.java
+++ app/src/main/java/fr/abouveron/projectamio/BootBroadcastReceiver.java
@@ -7,21 +7,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
+import android.widget.Toast;
 
 public class BootBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("MyBootBroadcastReceiver", "onReceive");
+        if (context == null) return;
+       // if (!MainActivity.getBootActivatedInConfig(context)) return;
 
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+        Intent serviceIntent = new Intent(context, MainService.class);
+        context.startForegroundService(serviceIntent);
+        Toast.makeText(context, "Service AMIO Lancé", Toast.LENGTH_LONG).show();
+
+
+        /*if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             SharedPreferences settings = context.getSharedPreferences("settings", MODE_PRIVATE);
             boolean startAtBoot = settings.getBoolean("startAtBoot", false);
 
             if (startAtBoot) {
                 Intent serviceIntent = new Intent(context, MainService.class);
                 context.startService(serviceIntent);
-            }
-        }
+            }*/
+
     }
 }

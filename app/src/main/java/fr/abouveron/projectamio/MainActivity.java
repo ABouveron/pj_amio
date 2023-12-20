114bf72cbee5876c6ba5d381d48e782b083c6974
unknown <hind.latrache@telecomnancy.eu>
2023-12-20 18:08:49 +0100

    Revert "test boot and service"
    This reverts commit e362bf9a71cd64880ba890e1a822a33609e2a7c7.

--- app/src/main/java/fr/abouveron/projectamio/MainActivity.java
+++ app/src/main/java/fr/abouveron/projectamio/MainActivity.java
@@ -1,25 +1,17 @@
 package fr.abouveron.projectamio;
 
-import android.annotation.SuppressLint;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.activity.ComponentActivity;
 import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
 import fr.abouveron.projectamio.Utilities.Notification;
@@ -37,19 +29,6 @@ public class MainActivity extends ComponentActivity {
 
         BroadcastReceiver br = new BootBroadcastReceiver();
         IntentFilter filter = new IntentFilter(Intent.ACTION_BOOT_COMPLETED);
-
-        createNotificationChannel();
-
-
-        @SuppressLint("ResourceType") View view = LayoutInflater.from(this).inflate(R.xml.preferences, null);
-        CheckBox checkBox = view.findViewById(R.id.checkbox);
-
-        checkBox.setOnCheckedChangeListener((compoundButton, isChecked) -> {
-            if (isChecked) {
-                startForegroundService(new Intent(MainActivity.this, MainService.class));
-                startService(serviceIntent);
-            }});
-
         boolean listenToBroadcastsFromOtherApps = true;
         int receiverFlags = listenToBroadcastsFromOtherApps
                 ? ContextCompat.RECEIVER_EXPORTED
@@ -77,22 +56,6 @@ public class MainActivity extends ComponentActivity {
 
         findViewById(Button.class, R.id.buttonSettings).setOnClickListener(v -> startActivity(settingsIntent));
     }
-
-    private void createNotificationChannel() {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                NotificationChannel channel = new NotificationChannel(
-                        "test",
-                        "YourChannelName",
-                        NotificationManager.IMPORTANCE_DEFAULT
-                );
-                NotificationManager manager = getSystemService(NotificationManager.class);
-                if (manager != null) {
-                    manager.createNotificationChannel(channel);
-                }
-            }
-        }
-
-
 
     @Override
     protected void onDestroy() {
